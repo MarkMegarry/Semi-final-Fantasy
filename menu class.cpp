11bc61eea960e9fe52b29019b801b8d22b13be37
MarkMegarry <30323234+MarkMegarry@users.noreply.github.com>
2017-08-21 12:33:50 +0100

    Update menu class.cpp

--- menu class.cpp
+++ menu class.cpp
@@ -3,5 +3,26 @@ using namespace std;
 
 int main()
 {
+ 
+  public:
+  //Decalaration of variables
+  //What the user wants to do
+  string action;
+  
+  //Displays main message to user
+  void mainPrompt()
+  {
+    cout << "What would you like to do?  Type \"help\" for a list of available actions" << endl;
+  }
+  //Sets action variable
+  void setAction()
+  {
+    cin >> action
+  }
+  //Gets action variable
+  string getAction()
+  {
+    return action;
+  }
   
 }
